1efdceb6bad2f250c69b27b3ff4dd7b778039f60
Bryan English <bryan.english@datadoghq.com>
2025-03-24 13:52:00 -0400

    deal with URLs passed to rewriter

--- js/module-details/index.js
+++ js/module-details/index.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
 const getDetails = require('module-details-from-path')
+const url = require('url')
 
 function getVersion (baseDir) {
+  if (baseDir instanceof URL || baseDir.startsWith('file://')) {
+    baseDir = url.fileURLToPath(baseDir)
+  }
   try {
     return JSON.parse(fs.readFileSync(`${baseDir}/package.json`, 'utf8')).version
   } catch (e) {
